45a5e2c021d7bd396394c423077a4fd48db31999
AqilCont <aqilcm@gmail.com>
2023-01-29 21:19:06 -0500

    Add some more links and resources to codegen comment doc

--- codegen.c
+++ codegen.c
@@ -4,14 +4,22 @@
 /*
 https://sonictk.github.io/asm_tutorial/
 https://stackoverflow.com/questions/36529449/why-are-rbp-and-rsp-called-general-purpose-registers
-* 16 Registers on an x64 cpu: rax, rbx, rcx, rdx, rbp, rsi, rdi, rsp, r8, r9, r10, r11 r12, r13, r14 and r15
-* rbp = base of stack frame, rsp = current place in stack
-* rax subsets:
-	* (a in rax) al (first 8), (a in rax) ah (second 8)
-	* (a in rax) ax(first word(16))
-	* eax(first 32 bits)
-	* Can be used for other registers ending in x
+https://stackoverflow.com/questions/892928/why-are-x86-registers-named-the-way-they-are
 
+* 14 General Purpose Registers on an x64 cpu: rax, rbx, rcx, rdx, rdi, rsi, r8, r9, r10, r11 r12, r13, r14 and r15
+	* AX: Accumulator, BX: Base, CX: Counter, DX: Data
+	* r_x subsets:
+		* _l (first 8), _h (second 8)
+		* _x(first word(16))
+		* e_x(first 32 bits)
+		* Can be used for other registers ending in x
+* 2 Special Purpose registers: rbp, rsp
+	* rbp: Stack base pointer
+	* rsp: Stack pointer
+* %rip = Instruction pointer.
+
+NOTES:
+	* lea %rax, [rel main]: moves the function ptr of main to %rax
 */
 
 
