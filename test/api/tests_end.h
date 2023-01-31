5c99b2a9bd1cbac43373fd4a7dc20270425b3621
AqilCont <aqilcm@gmail.com>
2023-01-30 21:01:09 -0500

    Finalize Testing Framework

--- test/api/tests_end.h
+++ test/api/tests_end.h
@@ -2,7 +2,10 @@
 
 int main() {
 	TESTFUNCRET (*testfuncs[])TESTFUNCARGS = { TESTFUNGEN() };
-	for (int i = 0, len = __COUNTER__ - 1; i < len; i ++) {
-		testfuncs[i]();
-	}
+	int failed = 0;
+	for (int i = 0, len = __COUNTER__ - 1; i < len; i ++)
+		if (testfuncs[i]()) failed ++;
+	if(!failed) puts("ALL TESTS PASSED!! Nice job bro.");
+	else if(failed == tests_run) puts("All tests failed. Spectacular.");
+	else printf(" %d / %d tests passed.", tests_run - failed, tests_run);
 }
