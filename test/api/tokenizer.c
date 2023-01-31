bcd4cf7afe51e5152843dcdf66ece23c34f89ba6
AqilCont <aqilcm@gmail.com>
2023-01-30 21:04:33 -0500

    Write a couple tests for Tokenizer

--- test/api/tokenizer.c
+++ test/api/tokenizer.c
@@ -1,10 +1,21 @@
 #include "../../tok.h"
 #include "tests.h"
 
+#undef TESTINIT
+#define TESTINIT struct Tokens* t = 
+#undef TESTCLEAN
+#define TESTCLEAN tokfree(t);
+
 TEST("Tokenize Single keyword")
-	struct Tokens* toks = tokenize("let");
-	struct Token* t = toks->toks;
-	assert(t->type == DECL);
+	tokenize("let");
+	assert(t->toks[0].type == DECL);
+	assert(vlen(t->toks) == 1);
+TEND()
+
+TEST("Tokenize Operator +")
+	tokenize("+");
+	assert(t->toks[0].type == OP);
+	assert(vlen(t->toks) == 1);
 TEND()
 
 #include "tests_end.h"
