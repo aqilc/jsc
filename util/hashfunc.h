bcf28d785447064d5d6efebb3172d3dfa2d66ea5
Aqil Contractor <aqilcm@gmail.com>
2023-04-16 18:04:24 -0400

    util/hashfunc: Use xxHash instead of qedhash. Initial testing shows 100%+ improvements in collision frequency and hashing performance.

--- util/hashfunc.h
+++ util/hashfunc.h
@@ -1,5 +1,6 @@
-// http://www.azillionmonkeys.com/qed/hash.html
+// Defined this file for a clear interface for the main hash function used in the project
 
+// http://www.azillionmonkeys.com/qed/hash.html
 #include "stdint.h" /* Replace with <stdint.h> if appropriate */
 uint32_t hash(const char * data, int len);
 
